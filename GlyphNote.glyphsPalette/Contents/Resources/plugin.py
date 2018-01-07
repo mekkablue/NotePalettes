50a2c2f21fbc54fd836a044528cb9107932f03de
Rainer Erich Scheichelbauer <res@glyphsapp.com>
2018-01-07 21:02:09 +0100

    Updated to latest codebase, added German localisation.

--- GlyphNote.glyphsPalette/Contents/Resources/plugin.py
+++ GlyphNote.glyphsPalette/Contents/Resources/plugin.py
@@ -9,20 +9,22 @@
 #
 #######################################################################################
 
-
+import objc
+from GlyphsApp import *
 from GlyphsApp.plugins import *
 
 class GlyphNote (PalettePlugin):
-	
 	dialogName = "com.mekkablue.GlyphNote"
 	dialog = objc.IBOutlet()
 	noteTextField = objc.IBOutlet()
 	
 	def settings(self):
-		self.name = Glyphs.localize({'en': u'Glyph Note', 'de': u'Glyphennotiz'})
-		"""
-		The minimum/maximum height of the view in pixels. 'max' must be bigger than 'min'.
-		"""
+		self.name = Glyphs.localize({
+			'en': u'Glyph Note',
+			'de': u'Glyphennotiz'
+		})
+		
+		# The minimum/maximum height of the view in pixels. 'max' must be bigger than 'min'.
 		self.min = 30
 		self.max = 700
 
@@ -38,73 +40,69 @@ class GlyphNote (PalettePlugin):
 	
 	@objc.IBAction
 	def setNote_(self, sender):
-		"""Sets the glyph note to whatever has been entered into the text field in the palette."""
-		try:
-			theseGlyphs = []
-			
-			# Extract font from sender
-			thisFont = self.windowController().document().font
+		"""
+		Sets the glyph note to whatever has been entered
+		into the text field in the palette.
+		"""
+		# Extract font from sender
+		thisFont = self.windowController().document().font
 
+		# We’re in the Edit View
+		if thisFont.currentTab:
+			theseGlyphs = [l.parent for l in thisFont.selectedLayers]
+		# We’re in the Font view
+		else:
+			theseGlyphs = [g for g in thisFont.selection]
+		
+		for thisGlyph in theseGlyphs:
+			thisGlyph.note = self.noteTextField.stringValue()
+	
+	def update(self, sender):
+		# only update if there is a window:
+		if self.windowController():
+			theseGlyphs = []
+			thisFont = self.windowController().document().font
+	
 			# We’re in the Edit View
 			if thisFont.currentTab:
 				theseGlyphs = [l.parent for l in thisFont.selectedLayers]
 			# We’re in the Font view
 			else:
 				theseGlyphs = [g for g in thisFont.selection]
-			
+
+			allNotes = []
 			for thisGlyph in theseGlyphs:
-				enteredText = self.noteTextField.stringValue()
-				thisGlyph.note = enteredText
-				
-		except Exception as e:
-			import traceback
-			self.logError(traceback.format_exc())
-	
-	def update(self, sender):
-		try:
-			# only update if there is a window:
-			if self.windowController():
-				theseGlyphs = []
-				thisFont = self.windowController().document().font
+				thisNote = thisGlyph.note
+				if thisNote == "":
+					thisNote = None
+				allNotes.append(thisNote)
+
+			numberOfDifferentNotes = len(set(allNotes))
+
+			# update glyph note in palette:
+			if numberOfDifferentNotes == 1:
+				self.noteTextField.setPlaceholderString_( Glyphs.localize({
+					'en': u'Empty glyph note%s.' % ("s" if len(theseGlyphs)>1 else ""),
+					'de': u'Leere Glyphennotiz%s.' % ("en" if len(theseGlyphs)>1 else ""),
+				}))
+				thisGlyphNote = theseGlyphs[0].note
+				if not thisGlyphNote:
+					thisGlyphNote = ""
+				self.noteTextField.setStringValue_(thisGlyphNote)
+			
+			elif numberOfDifferentNotes == 0:
+				self.noteTextField.setPlaceholderString_(Glyphs.localize({
+					'en': u'No glyph selected.',
+					'de': u'Keine Glyphe ausgewählt.',
+				}))
+				self.noteTextField.setStringValue_("")
 		
-				# We’re in the Edit View
-				if thisFont.currentTab:
-					theseGlyphs = [l.parent for l in thisFont.selectedLayers]
-				# We’re in the Font view
-				else:
-					theseGlyphs = [g for g in thisFont.selection]
-
-				allNotes = []
-				for thisGlyph in theseGlyphs:
-					thisNote = thisGlyph.note
-					if thisNote == "":
-						thisNote = None
-					allNotes.append(thisNote)
-
-				numberOfDifferentNotes = len(set(allNotes))
-
-				if numberOfDifferentNotes == 1:
-					# update glyph note in palette:
-					if len(theseGlyphs) > 1:
-						note = "notes"
-					else:
-						note = "note"
-					self.noteTextField.setPlaceholderString_("Empty glyph %s."%note)
-			
-					thisGlyphNote = theseGlyphs[0].note
-					if not thisGlyphNote:
-						thisGlyphNote = ""
-					self.noteTextField.setStringValue_(thisGlyphNote)
-				
-				elif numberOfDifferentNotes == 0:
-					self.noteTextField.setPlaceholderString_("No glyph selected.")
-					self.noteTextField.setStringValue_("")
-			
-				else:
-					self.noteTextField.setPlaceholderString_("Multiple values.")
-					self.noteTextField.setStringValue_("")
-		except:
-			self.logError(traceback.format_exc())
+			else:
+				self.noteTextField.setPlaceholderString_(Glyphs.localize({
+					'en': u'Multiple values.',
+					'de': u'Mehrere Werte.',
+				}))
+				self.noteTextField.setStringValue_("")
 	
 	def __file__(self):
 		"""Please leave this method unchanged"""
