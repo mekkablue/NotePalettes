6196a9de3802cfe73e9f490d182dccbea7fbf0ff
schriftgestalt <georg.seifert@mac.com>
2024-09-17 23:33:24 +0200

    code formatting

--- GlyphNote.glyphsPalette/Contents/Resources/plugin.py
+++ GlyphNote.glyphsPalette/Contents/Resources/plugin.py
@@ -4,23 +4,24 @@ from __future__ import division, print_function, unicode_literals
 ###########################################################################################################
 #
 #
-#	Palette Plugin
+# Palette Plugin
 #
-#	Read the docs:
-#	https://github.com/schriftgestalt/GlyphsSDK/tree/master/Python%20Templates/Palette
+# Read the docs:
+# https://github.com/schriftgestalt/GlyphsSDK/tree/master/Python%20Templates/Palette
 #
 #
 ###########################################################################################################
 
 import objc
-from GlyphsApp import *
-from GlyphsApp.plugins import *
+from GlyphsApp import Glyphs, UPDATEINTERFACE
+from GlyphsApp.plugins import PalettePlugin
+
 
 class GlyphNote (PalettePlugin):
 	dialogName = "com.mekkablue.GlyphNote"
 	dialog = objc.IBOutlet()
 	noteTextField = objc.IBOutlet()
-	
+
 	@objc.python_method
 	def settings(self):
 		self.name = Glyphs.localize({
@@ -29,14 +30,14 @@ class GlyphNote (PalettePlugin):
 			'es': 'Nota de glifo',
 			'fr': 'Notes du glyphe',
 		})
-		
+
 		# The minimum/maximum height of the view in pixels. 'max' must be bigger than 'min'.
 		self.min = 30
 		self.max = 700
 
 		# Load .nib dialog (without .extension)
 		self.loadNib('IBdialog', __file__)
-	
+
 	@objc.python_method
 	def start(self):
 		Glyphs.addCallback(self.update, UPDATEINTERFACE)
@@ -44,39 +45,39 @@ class GlyphNote (PalettePlugin):
 	@objc.python_method
 	def __del__(self):
 		Glyphs.removeCallback(self.update, UPDATEINTERFACE)
-	
+
 	@objc.IBAction
 	def setNote_(self, sender):
 		"""
 		Sets the glyph note to whatever has been entered
 		into the text field in the palette.
 		"""
-		
+
 		if self.windowController():
-			
+
 			# Extract font from sender
 			thisFont = self.windowController().document().font
 
 			# We’re in the Edit View
 			if thisFont.currentTab:
-				theseGlyphs = [l.parent for l in thisFont.selectedLayers]
+				theseGlyphs = [layer.parent for layer in thisFont.selectedLayers]
 			# We’re in the Font view
 			else:
 				theseGlyphs = [g for g in thisFont.selection]
-		
+
 			for thisGlyph in theseGlyphs:
 				thisGlyph.note = self.noteTextField.stringValue()
-	
+
 	@objc.python_method
 	def update(self, sender):
 		# only update if there is a window:
 		if self.windowController():
 			theseGlyphs = []
 			thisFont = self.windowController().document().font
-	
+
 			# We’re in the Edit View
 			if thisFont.currentTab:
-				theseGlyphs = [l.parent for l in thisFont.selectedLayers]
+				theseGlyphs = [layer.parent for layer in thisFont.selectedLayers]
 			# We’re in the Font view
 			else:
 				theseGlyphs = [g for g in thisFont.selection]
@@ -89,22 +90,22 @@ class GlyphNote (PalettePlugin):
 						thisNote = None
 					allNotes.append(thisNote)
 				except:
-					pass # can happen with control layers
+					pass  # can happen with control layers
 			numberOfDifferentNotes = len(set(allNotes))
 
 			# update glyph note in palette:
 			if numberOfDifferentNotes == 1:
-				self.noteTextField.setPlaceholderString_( Glyphs.localize({
-					'en': u'Empty glyph note%s.' % ("s" if len(theseGlyphs)>1 else ""),
-					'de': u'Leere Glyphennotiz%s.' % ("en" if len(theseGlyphs)>1 else ""),
-					'fr': u'Note%s vide%s.' % ("s" if len(theseGlyphs)>1 else "", "s" if len(theseGlyphs)>1 else ""),
-					'es': u'Nota%s vacía%s.' % ("s" if len(theseGlyphs)>1 else "", "s" if len(theseGlyphs)>1 else ""),
+				self.noteTextField.setPlaceholderString_(Glyphs.localize({
+					'en': u'Empty glyph note%s.' % ("s" if len(theseGlyphs) > 1 else ""),
+					'de': u'Leere Glyphennotiz%s.' % ("en" if len(theseGlyphs) > 1 else ""),
+					'fr': u'Note%s vide%s.' % ("s" if len(theseGlyphs) > 1 else "", "s" if len(theseGlyphs) > 1 else ""),
+					'es': u'Nota%s vacía%s.' % ("s" if len(theseGlyphs) > 1 else "", "s" if len(theseGlyphs) > 1 else ""),
 				}))
 				thisGlyphNote = allNotes[0]
 				if not thisGlyphNote:
 					thisGlyphNote = ""
 				self.noteTextField.setStringValue_(thisGlyphNote)
-			
+
 			elif numberOfDifferentNotes == 0:
 				self.noteTextField.setPlaceholderString_(Glyphs.localize({
 					'en': u'No glyph selected.',
@@ -113,7 +114,7 @@ class GlyphNote (PalettePlugin):
 					'es': u'Ningún glifo seleccionado.',
 				}))
 				self.noteTextField.setStringValue_("")
-		
+
 			else:
 				self.noteTextField.setPlaceholderString_(Glyphs.localize({
 					'en': u'Multiple values.',
@@ -122,7 +123,7 @@ class GlyphNote (PalettePlugin):
 					'es': u'Valores múltiples.',
 				}))
 				self.noteTextField.setStringValue_("")
-	
+
 	@objc.python_method
 	def __file__(self):
 		"""Please leave this method unchanged"""
