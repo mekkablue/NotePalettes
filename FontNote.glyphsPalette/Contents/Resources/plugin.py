b5bcbd298da4c71d4b9ecc9499ae79a3f2f48c09
schriftgestalt <georg.seifert@mac.com>
2024-09-17 23:36:51 +0200

    code formatting

--- FontNote.glyphsPalette/Contents/Resources/plugin.py
+++ FontNote.glyphsPalette/Contents/Resources/plugin.py
@@ -11,15 +11,16 @@ from __future__ import division, print_function, unicode_literals
 #######################################################################################
 
 import objc
-from GlyphsApp.plugins import *
-from GlyphsApp import UPDATEINTERFACE
+from GlyphsApp.plugins import PalettePlugin
+from GlyphsApp import Glyphs, UPDATEINTERFACE
+
 
 class FontNote (PalettePlugin):
-	
+
 	dialogName = "com.mekkablue.FontNote"
 	dialog = objc.IBOutlet()
 	noteTextField = objc.IBOutlet()
-	
+
 	@objc.python_method
 	def settings(self):
 		self.name = Glyphs.localize({
@@ -33,22 +34,22 @@ class FontNote (PalettePlugin):
 
 		# Load .nib dialog (without .extension)
 		self.loadNib('IBdialog', __file__)
-	
+
 	@objc.python_method
 	def start(self):
 		Glyphs.addCallback(self.update, UPDATEINTERFACE)
-	
+
 	@objc.python_method
 	def __del__(self):
 		Glyphs.removeCallback(self.update, UPDATEINTERFACE)
-	
+
 	@objc.IBAction
 	def setNote_(self, sender):
 		windowController = self.windowController()
 		if windowController:
 			thisFont = windowController.document().font
 			thisFont.note = self.noteTextField.stringValue()
-	
+
 	@objc.python_method
 	def update(self, sender):
 		# only update if there is a window:
@@ -60,7 +61,7 @@ class FontNote (PalettePlugin):
 				if not thisFontNote:
 					thisFontNote = ""
 				self.noteTextField.setStringValue_(thisFontNote)
-	
+
 	@objc.python_method
 	def __file__(self):
 		"""Please leave this method unchanged"""
